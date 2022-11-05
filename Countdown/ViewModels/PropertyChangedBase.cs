2325696cf6c999d3238524c0a750e4bb3009f94a
David Hancock <code@davidhancock.net>
2022-11-05 17:45:57 +0000

    Update default parameter

--- Countdown/ViewModels/PropertyChangedBase.cs
+++ Countdown/ViewModels/PropertyChangedBase.cs
@@ -4,12 +4,12 @@ abstract internal class PropertyChangedBase : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+    protected void RaisePropertyChanged([CallerMemberName] string? propertyName = default)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    protected bool HandlePropertyChanged<T>(ref T propertyValue, T newValue, [CallerMemberName] string propertyName = "")
+    protected bool HandlePropertyChanged<T>(ref T propertyValue, T newValue, [CallerMemberName] string? propertyName = default)
     {
         if ((propertyValue is null) || (!propertyValue.Equals(newValue)))
         {
