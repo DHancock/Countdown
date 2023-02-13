35a00f7e1287e8042ff47fba21813a83364efc01
David Hancock <code@davidhancock.net>
2023-02-13 16:16:40 +0000

    Simplify settings page clock images
    Instead of using images just use the clock itself.

--- Countdown/Views/SettingsView.xaml.cs
+++ Countdown/Views/SettingsView.xaml.cs
@@ -7,8 +7,6 @@ namespace Countdown.Views;
 /// </summary>
 internal sealed partial class SettingsView : Page
 {
-    bool firstLoad = true;
-
     public SettingsViewModel? ViewModel { get; set; }
 
     public SettingsView()
@@ -21,57 +19,5 @@ internal sealed partial class SettingsView : Page
         if (App.IsPackaged)
             VersionTextBlock.Text += " (P)";
 #endif
-
-        Loaded += async (s, e) =>
-        {
-            if (firstLoad)
-            {
-                firstLoad = false;
-
-                if (IsDarkTheme)
-                    AboutImageDark.Opacity = 1;
-                else
-                    AboutImageLight.Opacity = 1;
-
-                AboutImageLight.Source = await MainWindow.LoadEmbeddedImageResource("Countdown.Resources.256-light.png");
-                AboutImageDark.Source = await MainWindow.LoadEmbeddedImageResource("Countdown.Resources.256-dark.png");
-
-                // set duration for the next transition
-                LightFader.Duration = new TimeSpan(0, 0, 0, 0, 125);
-                DarkFader.Duration = new TimeSpan(0, 0, 0, 0, 125);
-            }
-
-            ActualThemeChanged += SettingsView_ActualThemeChanged;
-        };
-
-        Unloaded += (s, e) =>
-        {
-            ActualThemeChanged -= SettingsView_ActualThemeChanged;
-        };
-    }
-
-    private void SettingsView_ActualThemeChanged(FrameworkElement sender, object args)
-    {
-        if (IsDarkTheme)
-        {
-            AboutImageLight.Opacity = 0;
-            AboutImageDark.Opacity = 1;
-        }
-        else
-        {
-            AboutImageLight.Opacity = 1;
-            AboutImageDark.Opacity = 0;
-        }
-    }
-
-    private bool IsDarkTheme
-    {
-        get
-        {
-            if (ActualTheme == ElementTheme.Default)
-                return App.Current.RequestedTheme == ApplicationTheme.Dark;
-
-            return ActualTheme == ElementTheme.Dark;
-        }
     }
 }
